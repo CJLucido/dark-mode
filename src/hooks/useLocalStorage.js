52b3cd115dfea6198e01441f26247b0e38836e66
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-30 13:43:05 -0700

    adding localStorage functionality to useLocalStorage hook, still returns null

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,17 @@
 import {useState} from 'react'
 
-export default useLocalStorage(){
+export default function useLocalStorage(key, initialValue){
+    const [storedValue, setStoredValue] = useState(()=>{
+        if(window.localStorage.getItem(key)){
+            return JSON.parse(window.localStorage.getItem(key))
+        }
+        return(
+            initialValue
+        )
+    })
 
-
-    return(
+    return (
         null
     )
+    
 }
