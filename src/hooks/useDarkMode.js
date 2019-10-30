d21374022504cb867ef0b9448a54bc550287e21c
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-30 14:14:38 -0700

    step three completed?? if there is an issue with useDarkMode it is probably the key I set

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,16 @@
-import {useState, useEffect} from 'react'
+import {useEffect} from 'react'
 import useLocalStorage from "./useLocalStorage"
+
 
 
 export default function useDarkMode(){
     const[storedValues, setValues] = useLocalStorage('theUpsideDown')
 
 
-    return (
-        null
-    )
+    useEffect(()=> {
+        storedValues === true ? body.className.add('dark-mode') : body.className.remove('dark-mode');
+    }, [darkMode])
+
+    return [storedValues, setValues]
 }
 
